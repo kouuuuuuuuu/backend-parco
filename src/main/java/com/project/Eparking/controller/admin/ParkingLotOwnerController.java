749bdf439effab40188ba3edc4cd589d0d29a06a
quocbahuynh <jbrteamwebsite@gmail.com>
2023-10-17 15:08:38 +0700

    fix cors

--- src/main/java/com/project/Eparking/controller/admin/ParkingLotOwnerController.java
+++ src/main/java/com/project/Eparking/controller/admin/ParkingLotOwnerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
-@CrossOrigin()
+@CrossOrigin
 @RestController
 @RequestMapping("/plo")
 @RequiredArgsConstructor
